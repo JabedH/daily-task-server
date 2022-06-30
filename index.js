5de92d147e49bf78debb74cb0ee87aff4e82533e
JabedH <jabedhtusar@gmail.com>
2022-07-01 03:37:43 +0600

    added site to DB

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PRT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jptsq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const todolistCollection = client.db("todlist_data").collection("addlist");
+    app.get("/addlist", async (req, res) => {
+      const query = {};
+      const cursor = todolistCollection.find(query);
+      const allList = await cursor.toArray();
+      res.send(allList);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello FROM DAILY TASK!");
