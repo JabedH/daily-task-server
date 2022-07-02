1dad0a803f24a0919e473a7f288d8acd1bfecf6d
JabedH <jabedhtusar@gmail.com>
2022-07-02 17:55:06 +0600

    added new

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
@@ -40,19 +41,50 @@ async function run() {
       const tools = await todolistCollection.findOne(query);
       res.send(tools);
     });
+    // delete one
+    app.delete("/addlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await todolistCollection.deleteOne(query);
+      res.send(result);
+    });
     app.post("/addlist", async (req, res) => {
       const newList = req.body;
       const result = await todolistCollection.insertOne(newList);
       res.send(result);
     });
-    app.patch("/addlist/:id", async (req, res) => {
-      const email = req.query.email;
-      const info = req.body;
-      console.log(info, email);
-      const filter = { email: email };
-      const allCollection = await todolistCollection.updateOne(info, filter);
-      res.send(allCollection);
+    app.post("/complete", async (req, res) => {
+      const newList = req.body;
+      const result = await CompleteCollection.insertOne(newList);
+      res.send(result);
     });
+    app.put("/addlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateList = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: updateList.title,
+          date: updateList.date,
+          place: updateList.place,
+        },
+      };
+      const result = await todolistCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    // app.patch("/addlist/:id", async (req, res) => {
+    //   const email = req.query.email;
+    //   const info = req.body;
+    //   console.log(info, email);
+    //   const filter = { email: email };
+    //   const allCollection = await todolistCollection.updateOne(info, filter);
+    //   res.send(allCollection);
+    // });
   } finally {
   }
 }
