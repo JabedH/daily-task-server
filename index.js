2ab3854c7a924c6bbf4c20d7c7a4c3c011980d81
JabedH <jabedhtusar@gmail.com>
2022-07-15 23:34:40 +0600

    added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
@@ -10,21 +10,21 @@ const ObjectId = require("mongodb").ObjectId;
 app.use(express.json());
 app.use(cors());
 
-// const verifyJWT = (req, res, next) => {
-//   const authHeaders = req.headers.authorization;
-//   console.log("inside verify token", authHeaders);
-//   if (!authHeaders) {
-//     return res.status(401).send({ message: "unauthorized" });
-//   }
-//   const token = authHeaders.split(" ")[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-//     if (err) {
-//       return res.status(403).send({ message: "forbidden" });
-//     }
-//     req.decoded = decoded;
-//     next();
-//   });
-// };
+const verifyJWT = (req, res, next) => {
+  const authHeaders = req.headers.authorization;
+  console.log("inside verify token", authHeaders);
+  if (!authHeaders) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  const token = authHeaders.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jptsq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,21 +36,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const todolistCollection = client.db("todlist_data").collection("addlist");
     const CompleteCollection = client.db("todlist_data").collection("complete");
 
     // auth
-    // app.post("/login", async (req, res) => {
-    //   const user = req.body;
-    //   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN);
-    //   res.send({
-    //     success: true,
-    //     accessToken: accessToken,
-    //   });
-    // });
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN);
+      res.send({
+        success: true,
+        accessToken: accessToken,
+      });
+    });
 
-    app.get("/addlist", async (req, res) => {
+    app.get("/addlist", verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const cursor = todolistCollection.find(query);
